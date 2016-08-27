20fa55e9324a165ba32f6d4f3ea014784e2f14b7
Christopher Lyndon <chrislukelyndon@gmail.com>
2016-08-27 15:14:45 +0930

    Update Inchworm.hpp

--- WeekOne/Inchworm.hpp
+++ WeekOne/Inchworm.hpp
@@ -3,13 +3,14 @@
 #include <iostream>
 using namespace std;
 
-// inchworm simulator class.
 class Inchworm {
 public:
+  // Inputs: length of the branch, distance traveled by between rests, distance between each consecutive pair of leaves
+  // Outputs: number of leaves eaten.
+  // Moves an inchworm along a branch and eats a leaf if there happens to be one when it comes to a rest.
   int lunchtime(int branch, int rest, int leaf);
 };
 
-// Moves an inchworm along a branch and eats a leaf if there happens to be one when it comes to a rest.
 int Inchworm::lunchtime(int branch, int rest, int leaf) {
   // Vars
   int eatLeafCount = 1;
